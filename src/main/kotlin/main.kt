4f640418b603218af28fb10911b3d6d6829c8f4e
Ricardo Silva <ricardo.silva@sky.uk>
2020-12-03 19:45:40 +0000

    Puzzle 3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,10 @@
-import puzzle2.solvePuzzle2
-import puzzle2.solvePuzzle2dot1
+import puzzle3.solvePuzzle3
+import puzzle3.solvePuzzle3dot1
 
 fun main(args: Array<String>) {
-    println("Puzzle 2: ${solvePuzzle2()}")
-    println("Puzzle 2.1: ${solvePuzzle2dot1()}")
+    println("Puzzle 3: ${solvePuzzle3()}")
+    println("Puzzle 3.1: ${solvePuzzle3dot1()}")
+
+    // println("Puzzle 2: ${solvePuzzle2()}")
+    // println("Puzzle 2.1: ${solvePuzzle2dot1()}")
 }
--- src/main/kotlin/puzzle3/Puzzle3.kt
+++ src/main/kotlin/puzzle3/Puzzle3.kt
@@ -1,2 +1,87 @@
 package puzzle3
 
+import java.io.File
+
+data class Position(
+    var row: Int,
+    var column: Int
+)
+
+typealias Toboggan = Position
+
+data class Mountain(
+    val width: Int,
+    val trees: List<Position>
+)
+
+fun parseInput(filename: String): Mountain {
+    val trees = arrayListOf<Position>()
+    var row = 0
+    var column = 0
+
+    File("input/puzzle3/$filename").forEachLine { line ->
+        column = 0
+        line.forEach { char ->
+            if (char == '#') trees.add(Position(row, column))
+
+            column++
+        }
+        row++
+    }
+
+    return Mountain(column, trees)
+}
+
+fun solvePuzzle3(): Int {
+    val mountain = parseInput("input.txt")
+
+    val position = Position(0, 0)
+    val lowestTree = mountain.trees.last().row
+
+    var encounteredTrees = 0
+
+    while (true) {
+        position.row += 1
+        position.column += 3
+
+        if (position.row > lowestTree) return encounteredTrees
+
+        if (position.column >= mountain.width) position.column -= mountain.width
+        if (mountain.trees.contains(position)) encounteredTrees++
+    }
+}
+
+fun solvePuzzle3dot1(): Int {
+    val mountain = parseInput("input.txt")
+
+    val toboggans = arrayListOf<Toboggan>(
+        Toboggan(1, 1),
+        Toboggan(1, 3),
+        Toboggan(1, 5),
+        Toboggan(1, 7),
+        Toboggan(2, 1)
+    )
+
+    val lowestTree = mountain.trees.last().row
+
+    val encounteredTreesPerToboggan = arrayListOf<Int>()
+
+    toboggans.forEach { toboggan ->
+        val position = Position(0, 0)
+        var encounteredTrees = 0
+        while (true) {
+            position.row += toboggan.row
+            position.column += toboggan.column
+
+            if (position.row > lowestTree) {
+                encounteredTreesPerToboggan.add(encounteredTrees)
+                break
+            }
+
+            if (position.column >= mountain.width) position.column -= mountain.width
+            if (mountain.trees.contains(position)) encounteredTrees++
+        }
+    }
+
+    return encounteredTreesPerToboggan.reduce { acc, trees -> acc * trees }
+}
