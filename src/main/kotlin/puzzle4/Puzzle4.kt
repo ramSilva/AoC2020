1268331ac7b2b6746bcc53168e7317ef5dc93db0
Ricardo Silva <ricardo.silva@sky.uk>
2020-12-05 18:16:45 +0000

    Remove ignored files

--- src/main/kotlin/puzzle4/Puzzle4.kt
+++ src/main/kotlin/puzzle4/Puzzle4.kt
@@ -16,14 +16,81 @@ val expectedKeys = arrayListOf(
     "pid"
 )
 
+abstract class PassportValidator {
+    abstract fun isValid(passport: String): Boolean
+}
+
+class EnglishPassportValidator : PassportValidator() {
+    override fun isValid(passport: String) =
+        expectedKeys.size == expectedKeys.count { key ->
+            val regex = Regex("""$key$keyValueDelimiter""")
+            regex.containsMatchIn(passport)
+        }
+}
+
+class GermanPassportValidator : PassportValidator() {
+    override fun isValid(passport: String): Boolean {
+        return true
+    }
+}
+
+enum class PassportEntry {
+    byr {
+        override fun isValid(value: String): Boolean {
+            val validationRegex = Regex("""\d{4}""")
+            return (validationRegex.matches(value) && value.toInt() >= 1920 && value.toInt() <= 2002)
+        }
+    },
+    iyr {
+        override fun isValid(value: String): Boolean {
+            val validationRegex = Regex("""\d{4}""")
+            return (validationRegex.matches(value) && value.toInt() >= 2010 && value.toInt() <= 2020)
+        }
+    },
+    eyr {
+        override fun isValid(value: String): Boolean {
+            val validationRegex = Regex("""\d{4}""")
+            return (validationRegex.matches(value) && value.toInt() >= 2020 && value.toInt() <= 2030)
+        }
+    },
+    hgt {
+        override fun isValid(value: String): Boolean {
+            val validationRegex = Regex("""(\d+)(in|cm)""")
+            if (validationRegex.matches(value)) {
+                val (height, units) = validationRegex.find(value)!!.destructured
+
+                return (units == "cm" && height.toInt() >= 150 && height.toInt() <= 193) || (units == "in" && height.toInt() >= 59 && height.toInt() <= 76)
+            }
+
+            return false
+        }
+    },
+    hcl {
+        override fun isValid(value: String): Boolean {
+            val validationRegex = Regex("""#[0-9|a-f]{6}""")
+            return validationRegex.matches(value)
+        }
+    },
+    ecl {
+        override fun isValid(value: String): Boolean {
+            return (value == "amb" || value == "blu" || value == "brn" || value == "gry" || value == "grn" || value == "hzl" || value == "oth")
+        }
+    },
+    pid {
+        override fun isValid(value: String): Boolean {
+            val validationRegex = Regex("""\d{9}""")
+            return validationRegex.matches(value)
+        }
+    };
+
+    abstract fun isValid(value: String): Boolean
+}
+
 val inputText = File("input/puzzle4/$filename").readText()
 val passports = inputText.split(passportsDelimiter)
 
 fun solvePuzzle4() = passports.count { passport ->
-    expectedKeys.size == expectedKeys.count { key ->
-        val regex = Regex("""$key$keyValueDelimiter""")
-        regex.containsMatchIn(passport)
-    }
+    EnglishPassportValidator().isValid(passport)
 }
 
 fun solvePuzzle4dot1(): Int {
@@ -34,55 +101,9 @@ fun solvePuzzle4dot1(): Int {
 
             if (regex.containsMatchIn(passport)) {
                 val (_, value) = regex.find(passport)!!.destructured
+                val passportEntry = PassportEntry.valueOf(key)
 
-                when (key) {
-                    "byr" -> {
-                        val validationRegex = Regex("""\d{4}""")
-                        if (validationRegex.matches(value) && value.toInt() >= 1920 && value.toInt() <= 2002) {
-                            valid = true
-                        }
-                    }
-                    "iyr" -> {
-                        val validationRegex = Regex("""\d{4}""")
-                        if (validationRegex.matches(value) && value.toInt() >= 2010 && value.toInt() <= 2020) {
-                            valid = true
-                        }
-                    }
-                    "eyr" -> {
-                        val validationRegex = Regex("""\d{4}""")
-                        if (validationRegex.matches(value) && value.toInt() >= 2020 && value.toInt() <= 2030) {
-                            valid = true
-                        }
-                    }
-                    "hgt" -> {
-                        val validationRegex = Regex("""(\d+)(in|cm)""")
-                        if (validationRegex.matches(value)) {
-                            val (height, units) = validationRegex.find(value)!!.destructured
-
-                            if (units == "cm" && height.toInt() >= 150 && height.toInt() <= 193) {
-                                valid = true
-                            }
-                            if (units == "in" && height.toInt() >= 59 && height.toInt() <= 76) {
-                                valid = true
-                            }
-                        }
-                    }
-                    "hcl" -> {
-                        val validationRegex = Regex("""#[0-9|a-f]{6}""")
-                        if (validationRegex.matches(value)) {
-                            valid = true
-                        }
-                    }
-                    "ecl" -> if (value == "amb" || value == "blu" || value == "brn" || value == "gry" || value == "grn" || value == "hzl" || value == "oth") {
-                        valid = true
-                    }
-                    "pid" -> {
-                        val validationRegex = Regex("""\d{9}""")
-                        if (validationRegex.matches(value)) {
-                            valid = true
-                        }
-                    }
-                }
+                valid = passportEntry.isValid(value)
             }
             valid
         }
