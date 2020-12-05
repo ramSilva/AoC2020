c38eecddc1900692df545cfb1c46f5b2e1f6abce
Ricardo Silva <ricardo.silva@sky.uk>
2020-12-05 20:09:49 +0000

    Even more golfed 5

--- src/main/kotlin/puzzle5/Puzzle5.kt
+++ src/main/kotlin/puzzle5/Puzzle5.kt
@@ -4,7 +4,5 @@ import java.io.File
 
 const val f = "input/puzzle5/testInput.txt"
 
-fun solvePuzzle5mesmoàMacho() = Integer.valueOf(
-    File(f).readText().split('\n')
-        .maxOf { it.fold(0) { a, c -> a * 10 + if (c == 'B' || c == 'R') 1 else 0 } }.toString(), 2
-)
+fun solvePuzzle5mesmoàMacho() =
+    File(f).readLines().maxOf { it.fold("") { a, c -> a + if (c == 'B' || c == 'R') "1" else "0" }.toInt(2) }
